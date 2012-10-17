e0dbdcc05f2a47c1dec3574154fd57e4e4fd7b24
Peter Kehler <peter.kehler.jr@googlemail.com>
2012-10-17 19:03:02 +0200

    Updated installation instructions

---
+++ install_RcppGO.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+
+## find directory of file
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+cd $ABSPATH
+
+FILENAME=RcppGO
+
+# export librarys to PATH
+RCPP_CXXFLAGS=`Rscript -e 'Rcpp:::CxxFlags()'`
+RCPP_LIBS=`Rscript -e 'Rcpp:::LdFlags()'`
+RCPP_ARMADILLO=`Rscript -e 'RcppArmadillo:::CxxFlags()'`
+
+
+export PKG_CPPFLAGS="${RCPP_ARMADILLO} ${RCPP_CXXFLAGS}"
+export PKG_LIBS="-larmadillo -llapack  ${RCPP_LIBS}"
+
+R CMD INSTALL RcppGO*
